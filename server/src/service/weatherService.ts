b2584997819ef5c3eebb49fdb366b1258006ccf2
Fortis Irdmousa <moosorkh@gmail.com>
2024-09-16 23:09:28 -0700

    Normalize line endings and track necessary files

--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -2,14 +2,52 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // TODO: Define an interface for the Coordinates object
-
+interface Coordinates {
+  lat: number;
+  lon: number;
+}
 // TODO: Define a class for the Weather object
 
 // TODO: Complete the WeatherService class
 class WeatherService {
+  temp: number;
+  humidity: number;
+  windSpeed: number;
+  description: string;
+  icon: string;
+
+  constructor(
+    temp: number,
+    humidity: number,
+    windSpeed: number,
+    description: string,
+    icon: string
+  ) {
+    this.temp = temp;
+    this.humidity = humidity;
+    this.windSpeed = windSpeed;
+    this.description = description;
+    this.icon = icon;
+  }
+
   // TODO: Define the baseURL, API key, and city name properties
+  private async fetchLocationData(city: string): Promise<Coordinates> {
+    const response = await fetch(
+      `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.API_KEY}`
+    );
+    const data = await response.json();
+    const { lat, lon } = data[0];
+    return { lat, lon };
+  }
   // TODO: Create fetchLocationData method
   // private async fetchLocationData(query: string) {}
+  private async fetchWeatherData(coordinates: Coordinates): Promise<any> {
+    const { lat, lon } = coordinates;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&units=imperial`;
+    const response = await fetch(weatherUrl);
+    const weatherData = await response.json();
+    return weatherData;
+  }
   // TODO: Create destructureLocationData method
   // private destructureLocationData(locationData: Coordinates): Coordinates {}
   // TODO: Create buildGeocodeQuery method
